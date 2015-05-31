ba78336fc90074b09e79fba4df2b190aa7a5451a
Sam David <sfd1987@gmail.com>
2015-05-30 21:08:13 -0700

    a few more string algos

--- hackerrank_algorithms_strings.js
+++ hackerrank_algorithms_strings.js
@@ -21,3 +21,87 @@ function funnyString(input) {
 }
 
 funnyString("2\nacxz\nbcxz")
+
+// takes input and returns whether or not it is a pangram, contains all letters of the alphabet at least ones
+// treats upper and lowercase as the same
+// PASSED: 20/20
+function pangrams(input) {
+	var letters = input.replace(/\s/g,'').toLowerCase().split("");
+	var letterObject = {};
+	var totalLetters = 0;
+	for (var i=0;i<letters.length;i++) {
+		if (letterObject.hasOwnProperty(letters[i])) {
+			letterObject[letters[i]]++;
+		} else {
+			letterObject[letters[i]] = 1;
+			totalLetters++;
+		}
+	}
+	if (totalLetters === 26) {
+		console.log("pangram");
+	} else {
+		console.log("not pangram")
+	}
+}
+
+pangrams("We promptly judged antique ivory buckles for the next prize")
+pangrams("We promptly judged antique ivory buckles for the prize")
+
+// string only pass when they have alternate chars ABABA
+// find the minimum number of deletions to get rid of any repeating consecutive chars
+// PASSED: 30/30, nice simple solution
+function alternatingCharacters(input) {
+	var strings = input.split("\n");
+	strings.shift();
+	for (var i=0;i<strings.length;i++) {
+		var deletionsRequired = 0;
+		for (var j=0;j<strings[i].length;j++) {
+			if (strings[i][j] == strings[i][j+1]) {
+				deletionsRequired++;
+			}
+		}
+		console.log(deletionsRequired);
+	}
+}
+
+alternatingCharacters("5\nAAAA\nBBBBB\nABABABAB\nBABABA\nAAABBB")
+
+// figure out whether string input is an anagram of a palindrome
+function gameOfThrones(input) {
+	var codedString = input.split("");
+	var letterObject = {};
+	var letterCount = 0;
+	var oneOdd = false;
+	var isSolo = false;
+	var passedTest = true;
+	for (i=0;i<codedString.length;i++) {
+		if (letterObject.hasOwnProperty(codedString[i])) {
+			letterObject[codedString[i]]++;
+		} else {
+			letterObject[codedString[i]] = 1;
+			letterCount++;
+		}
+	}
+	for (letter in letterObject) {
+		if (letterObject[letter] % 2 != 0) {
+			if(oneOdd == false) {oneOdd=true
+			} else if (oneOdd == true) {
+				console.log("NO")
+				passedTest = false;
+				break;
+			}
+			if(letterObject[letter] == 1 && isSolo == false) {
+				isSolo = true;
+			} else if (letterObject[letter] == 1 && isSolo == true) {
+				console.log("NO");
+				passedTest = false;
+				break;
+			}
+		}
+	}
+	if (passedTest) {console.log("YES")}
+}
+
+gameOfThrones("aaabbbb")
+gameOfThrones("cdefghmnopqrstuvw")
+gameOfThrones("cdcdcdcdeeeef")
